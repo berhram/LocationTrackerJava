9bfba1a42542f3582d3916d7712ab58383be11e1
Daniil <berhram@protonmail.com>
2022-03-09 20:24:32 +0300

    # FirebaseAuthNetwork fix

--- core/src/main/java/com/velvet/core/models/auth/FirebaseAuthNetwork.java
+++ core/src/main/java/com/velvet/core/models/auth/FirebaseAuthNetwork.java
@@ -1,13 +1,13 @@
 package com.velvet.core.models.auth;
 
-import android.util.Log;
-
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.velvet.core.Values;
 import com.velvet.core.result.Result;
+
+import java.util.concurrent.ExecutionException;
 
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
@@ -31,44 +31,18 @@ public class FirebaseAuthNetwork implements AuthNetwork {
         return Single.fromCallable(() -> {
             if (Values.LOGIN.equals(message.id)) {
                 final Task<AuthResult> task = firebaseAuth.signInWithEmailAndPassword(message.emailOrCode, message.pwd);
-                Tasks.await(task);
-                if (task.isSuccessful()) {
-                    Log.d("AUTH", "taskValidator: Success");
-                    return Result.success(Values.LOGIN);
-                } else {
-                    Log.d("AUTH", "taskValidator: Failure");
-                    return new Result<>(Values.LOGIN, task.getException());
-                }
+                return completeTask(task, message.id);
             } else if (Values.REGISTER.equals(message.id)) {
                 final Task<AuthResult> task = firebaseAuth.createUserWithEmailAndPassword(message.emailOrCode, message.pwd);
                 Tasks.await(task);
-                if (task.isSuccessful()) {
-                    Log.d("AUTH", "taskValidator: Success");
-                    return Result.success(Values.REGISTER);
-                } else {
-                    Log.d("AUTH", "taskValidator: Failure");
-                    return new Result<>(Values.REGISTER, task.getException());
-                }
+                return completeTask(task, message.id);
             } else if (Values.REQUEST.equals(message.id)) {
                 final Task<Void> task = firebaseAuth.sendPasswordResetEmail(message.emailOrCode);
                 Tasks.await(task);
-                if (task.isSuccessful()) {
-                    Log.d("AUTH", "taskValidator: Success");
-                    return Result.success(Values.REQUEST);
-                } else {
-                    Log.d("AUTH", "taskValidator: Failure");
-                    return new Result<>(Values.REQUEST, task.getException());
-                }
+                return completeTask(task, message.id);
             } else if (Values.CHECK.equals(message.id)) {
                 final Task<Void> task = firebaseAuth.confirmPasswordReset(message.emailOrCode, message.pwd);
-                Tasks.await(task);
-                if (task.isSuccessful()) {
-                    Log.d("AUTH", "taskValidator: Success");
-                    return Result.success(Values.CHECK);
-                } else {
-                    Log.d("AUTH", "taskValidator: Failure");
-                    return new Result<>(Values.CHECK, task.getException());
-                }
+                return completeTask(task, message.id);
             } else {
                 return Result.error(new Exception("Wrong message id!"));
             }
@@ -78,5 +52,14 @@ public class FirebaseAuthNetwork implements AuthNetwork {
     @Override
     public Completable signOut() {
         return Completable.fromRunnable(firebaseAuth::signOut);
+    }
+
+    private Result<String> completeTask(Task task, String key) throws ExecutionException, InterruptedException {
+        Tasks.await(task);
+        if (task.isSuccessful()) {
+            return Result.success(key);
+        } else {
+            return new Result<>(key, task.getException());
+        }
     }
 }
